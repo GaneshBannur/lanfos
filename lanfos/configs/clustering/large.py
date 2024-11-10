41e6c426e2b103ac9d1c45471ee14aebe4f4a84e
GaneshBannur <76954924+GaneshBannur@users.noreply.github.com>
2024-11-10 13:42:27 -0800

    add clustering configs

---
+++ lanfos/configs/clustering/large.py
@@ -0,0 +1,5 @@
+dataset_path: str   # Path to dataset's top level folder
+num_clusters = 64
+feature_level = "large"
+data_dir = f"{dataset_path}/siglip_features"
+output_dir = f"{dataset_path}/clusters_{feature_level}"
---
+++ lanfos/configs/clustering/medium.py
@@ -0,0 +1,5 @@
+dataset_path: str   # Path to dataset's top level folder
+num_clusters = 192
+feature_level = "medium"
+data_dir = f"{dataset_path}/siglip_features"
+output_dir = f"{dataset_path}/clusters_{feature_level}"
