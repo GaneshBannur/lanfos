fdc910c0f4b86b9697024f6d4657ff4ac2f89bbb
GaneshBannur <76954924+GaneshBannur@users.noreply.github.com>
2024-11-10 13:43:14 -0800

    update train configs

--- lanfos/configs/lf_train/medium.py
+++ lanfos/configs/lf_train/medium.py
@@ -1,16 +1,12 @@
-# Convenience definitions
+save_path: str  # Path where trained model will be saved
+source_path: str    # Path to dataset's top level folder
+gs_ckpt_path: str   # Path to .pth file of RGB Gaussian Splatting
 dataset_name = "figurines"
-run_name = "lanfos_test"
-gs_ckpt_iter = 30_000
 test_frequency = 4_200
-
-# Actual runtime inputs
-lf_feature_level = "medium"
-lf_dir = f"/scratch/ganesh/3d_grounding/data/lerf_ovs/{dataset_name}/language_features_siglip_replaced"
-source_path = f"/scratch/ganesh/3d_grounding/data/lerf_ovs/{dataset_name}"
-model_path = f"/scratch/ganesh/3d_grounding/runs/{run_name}/{dataset_name}/{dataset_name}_{lf_feature_level}"
-lf_clusters_path = f"/scratch/ganesh/3d_grounding/lanfos/clustering/siglip/{dataset_name}/clustering_{lf_feature_level}/cluster_centers.npy"
-gs_ckpt_path = f"/scratch/ganesh/3d_grounding/runs/3dgs_no_test/{dataset_name}/chkpnt{gs_ckpt_iter}.pth"
+feature_level = "medium"
+lf_dir = f"{source_path}/siglip_features"
+model_path = f"{save_path}/{dataset_name}/{feature_level}"
+lf_clusters_path = f"{source_path}/clusters_{feature_level}/cluster_centers.npy"
 iterations = 52_500
 test_iterations = [1]+[test_frequency*i for i in range(1, iterations//test_frequency)] + [iterations]
 save_iterations = [iterations]
@@ -24,7 +20,7 @@ lf_lr = 1e-3
 lf_decoder_batch_size = 128
 data_device = "cpu"
 eval = True
-test_cam_names_path = f"/scratch/ganesh/3d_grounding/data/lerf_ovs/label/{dataset_name}/test_cam_names.json"
+test_cam_names_path = f"{source_path}/test_cam_names.json"
 lf_dim = 3
 num_lf_iter = 300
 num_lf_decoder_iter = 750
